8671fee54aec9b90acfb12f0be832afe68a81881
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-03-24 18:04:40 +1100

    Create new webpack config for non-server javascript

--- webpack.server.config.js
+++ webpack.server.config.js
@@ -1,48 +1,44 @@
-const path = require("path")
+const path = require('path')
 const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 module.exports = {
   entry: {
-    main: './index.js'
+    server: './server.js',
   },
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '/',
     filename: '[name].js'
   },
-  target: 'web',
-  devtool: '#source-map',
+  target: 'node',
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false,   // if you don't put this is, __dirname
+    __filename: false,  // and __filename return blank or /
+  },
+  externals: [nodeExternals()], // Need this to avoid error when working with Express
   module: {
     rules: [
       {
+        // Transpiles ES6-8 into ES5
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        use: {
+          loader: "babel-loader"
+        }
       },
       {
         // Loads the javacript into html template provided.
         // Entry point is set below in HtmlWebPackPlugin in Plugins 
         test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-            //options: { minimize: true }
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-       test: /\.(png|svg|jpg|gif)$/,
-       use: ['file-loader']
+        use: [{loader: "html-loader"}]
       }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/html/index.html",
+      template: "./index.html",
       filename: "./index.html",
       excludeChunks: [ 'server' ]
     })
