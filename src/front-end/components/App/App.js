d567304c2238c8e56dd5f13124be3b33754e4fa8
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-03-26 12:55:30 +1100

    Add images for later testing

--- src/front-end/components/App/App.js
+++ src/front-end/components/App/App.js
@@ -4,6 +4,7 @@ import './styles.css'
 const App = () => (
   <div>
     <h1>React Works</h1>
+    <div className='selfie'></div>
     <p>Check if snapshot breaks</p>
     <h3>Next check</h3>
   </div>
