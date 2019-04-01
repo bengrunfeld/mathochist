142133abfd41fa0a39c2478a9b4ae7eff3090165
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-04-02 09:39:38 +1100

    Add components for main pages

--- src/front-end/components/Page/Page.js
+++ src/front-end/components/Page/Page.js
@@ -1,16 +1,23 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import Post from '../Post'
 import About from '../About'
+import HomePage from '../HomePage'
+import Projects from '../Projects'
+import NotFound404 from '../NotFound404'
 
 
 const Page = () => (
   <section className='col-11 col-lg-9 offset-lg-1 pt-5 mb-5'>
-    <Route exact path='/page/about' component={About} />
-    <Route exact path='/post/react' render={() => <Post postId='react' />} />
-    <Route exact path='/post/redux' render={() => <Post postId='redux' />} />
-    <Route exact path='/post/immutable' render={() => <Post postId='immutable' />} />
-    {/* <Route path='/project' component={Project} /> */}
+    <Switch>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/page/about' component={About} />
+      <Route path='/page/projects' component={Projects} />
+      <Route exact path='/post/react' render={() => <Post postId='react' />} />
+      <Route exact path='/post/redux' render={() => <Post postId='redux' />} />
+      <Route exact path='/post/immutable' render={() => <Post postId='immutable' />} />
+      <Route component={NotFound404} />
+    </Switch>
   </section>
 )
 
