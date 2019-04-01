dbe88259e6369dacad6add8f747100e59ea14a4e
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-04-01 20:30:14 +1100

    Remove comments

--- src/server/router.js
+++ src/server/router.js
@@ -11,7 +11,6 @@ app.use(express.static(DIST_DIR))
 
 // Markdown docs
 router.get('/api/posts/:postId', (req, res) => {
-  console.log('\n-->> 1.', req.params)
   const content = fs.readFileSync(`./src/content/${req.params.postId}.md`, 'utf8')
 
   res.setHeader('Content-Type', 'application/json')
@@ -19,21 +18,13 @@ router.get('/api/posts/:postId', (req, res) => {
 })
 
 // Main pages - e.g. /about
-router.get('/page/:page', (req, res) => { 
-  console.log('\n-->> 2.', req.params)
-  res.sendFile(HTML_FILE) 
-})
+router.get('/page/:page', (req, res) => { res.sendFile(HTML_FILE) })
 
 // Post pages - e.g. /posts/redux
-router.get('/posts/:post', (req, res) => { 
-  console.log('\n-->> 3.', req.params)
-  res.sendFile(HTML_FILE) 
-})
+router.get('/posts/:post', (req, res) => { res.sendFile(HTML_FILE) })
 
 // Assets from the Dist folder - e.g. font files
 router.get('*', (req, res) => {
-  console.log('-->> 4.', req.params)
-
   if (req.params['0'] === '/' || req.params['0'].slice(0, 2) === '..')
     return res.sendFile(HTML_FILE)
   
