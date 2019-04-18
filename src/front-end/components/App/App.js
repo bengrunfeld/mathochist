4a53bb477268efc45a2edd8aac61aec862957499
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-04-18 14:27:22 +1000

    Install immutable and test that it works

--- src/front-end/components/App/App.js
+++ src/front-end/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../Navbar'
 import PageLayout from '../PageLayout'
 import Footer from '../Footer'
+import { Map } from 'immutable'
 
 import './styles.css'
 
@@ -13,12 +14,19 @@ import '../../assets/css/typography.css'
 import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 
-const App = () => (
+const App = () => {
+  const map1 = Map({ a: 1, b: 2, c: 3 })
+  const map2 = map1.set('b', 50)
+  console.log('----------->>> App: Immutable')
+  console.log(map1.get('b') + " vs. " + map2.get('b')) // 2 vs. 50
+
+  return (
   <div>
     <Navbar />
     <PageLayout />
     <Footer />
   </div>
-)
+  )
+}
 
 export default App
