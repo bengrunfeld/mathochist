42d9f899fdbaf77f28840ad706b81654d4ea8977
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-07-13 16:36:21 +1000

    Solve equation using arithmetic operators

--- src/front-end/utils/solveEquation.js
+++ src/front-end/utils/solveEquation.js
@@ -1,7 +1,12 @@
-const solveEquation = equation => {
-  const solution = 2 * 2
+const solveEquation = e => {
+  if (e.operator === '*') return e.firstArg * e.secondArg
+  if (e.operator === '/') return e.firstArg / e.secondArg
+  if (e.operator === '-') return e.firstArg - e.secondArg
+  if (e.operator === '+') return e.firstArg + e.secondArg
 
-  return solution
+  throw 'ERROR: solveEquation - no valid operator given'
 }
 
 export default solveEquation
+
+// e stands for equation. Just easier this way on the typey typey ;)
