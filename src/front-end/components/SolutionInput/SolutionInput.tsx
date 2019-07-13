5f9a0f5973f3d831028e5a997f801b43bbee3963
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-07-13 17:02:43 +1000

    Submit solution if user presses Enter in solutionInput box

--- src/front-end/components/SolutionInput/SolutionInput.tsx
+++ src/front-end/components/SolutionInput/SolutionInput.tsx
@@ -2,31 +2,47 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import updateSolutionInput from '../../state/solutionInput/actions'
-// import submitSolution from '../../state/answer/actions'
+import updateResult from '../../state/result/actions'
+import solveEquation from '../../utils/solveEquation'
 
 import { BoxContainer, Solution } from './styles'
 
-const handleKeyPress = (key, val) => {
+const handleKeyPress = (key, solutionInput, equation, updateResult) => {
   if (key === 'Enter') {
-    console.log('>>> Submit Solution', val)
-    // submitSolution()
+    if (solutionInput === '') return
+
+    const correctSolution = solveEquation(equation)
+    const result = correctSolution.toString() === solutionInput
+
+    updateResult(result)
   }
 }
 
-const SolutionInput = ({ width, solutionInput, updateSolutionInput }) => (
+const SolutionInput = ({
+  width,
+  solutionInput,
+  updateSolutionInput,
+  updateResult,
+  equation
+}) => (
   <BoxContainer className={width}>
     <Solution
       type='text'
       value={solutionInput}
       placeholder='Enter solution...'
       onChange={e => updateSolutionInput(e.target.value)}
-      onKeyPress={e => handleKeyPress(e.key, e.target.value)}
+      onKeyPress={e =>
+        handleKeyPress(e.key, e.target.value, equation, updateResult)
+      }
     />
   </BoxContainer>
 )
 
-const mapStateToProps = state => ({ solutionInput: state.solutionInput })
-const mapDispatchToProps = { updateSolutionInput }
+const mapStateToProps = state => ({
+  solutionInput: state.solutionInput,
+  equation: state.equation
+})
+const mapDispatchToProps = { updateSolutionInput, updateResult }
 
 SolutionInput.defaultProps = {
   width: 'col-8'
@@ -34,7 +50,8 @@ SolutionInput.defaultProps = {
 
 SolutionInput.propTypes = {
   width: PropTypes.string,
-  solutionInput: PropTypes.string
+  solutionInput: PropTypes.string,
+  equation: PropTypes.object
 }
 
 export default connect(
