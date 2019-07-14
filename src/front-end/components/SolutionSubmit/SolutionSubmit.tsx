938ce8617b76922f35e7148deb580589633d5a92
bengrunfeld <benjamin.grunfeld@gmail.com>
2019-07-15 00:00:21 +1000

    Increment score if User gets the right answer

--- src/front-end/components/SolutionSubmit/SolutionSubmit.tsx
+++ src/front-end/components/SolutionSubmit/SolutionSubmit.tsx
@@ -3,9 +3,16 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { BoxContainer, BoxButton, Triangle } from './styles'
 import updateResult from '../../state/result/actions'
+import updateScore from '../../state/score/actions'
 import solveEquation from '../../utils/solveEquation'
 
-const SolutionSubmit = ({ width, equation, solutionInput, dispatch }) => (
+const SolutionSubmit = ({
+  width,
+  equation,
+  solutionInput,
+  dispatch,
+  score
+}) => (
   <BoxContainer className={width}>
     <BoxButton
       className='float-right'
@@ -13,25 +20,31 @@ const SolutionSubmit = ({ width, equation, solutionInput, dispatch }) => (
         this,
         dispatch,
         equation,
-        solutionInput
+        solutionInput,
+        score
       )}>
       <Triangle />
     </BoxButton>
   </BoxContainer>
 )
 
-const handleSubmitSolution = (dispatch, equation, solutionInput) => {
+const handleSubmitSolution = (dispatch, equation, solutionInput, score) => {
   if (solutionInput === '') return
 
   const correctSolution = solveEquation(equation)
   const result = correctSolution.toString() === solutionInput
 
   dispatch(updateResult(result))
+
+  console.log('>>> S:', score)
+
+  if (result) dispatch(updateScore(score + 1))
 }
 
 const mapStateToProps = state => ({
   equation: state.equation,
-  solutionInput: state.solutionInput
+  solutionInput: state.solutionInput,
+  score: state.score
 })
 
 SolutionSubmit.defaultProps = {
@@ -41,7 +54,8 @@ SolutionSubmit.defaultProps = {
 SolutionSubmit.propTypes = {
   equation: PropTypes.object,
   width: PropTypes.string,
-  solutionInput: PropTypes.string
+  solutionInput: PropTypes.string,
+  score: PropTypes.number
 }
 
 export default connect(mapStateToProps)(SolutionSubmit)
